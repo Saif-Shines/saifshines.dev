1e8a7d4bd3d8647cd782be89f954896316971066
Saif Ali Shaik <saifshine7@gmail.com>
2023-09-16 20:18:08 +0530

    👌 IMPROVE: title and description

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-<h1>Hi from Saif!</h1>
+  <div class="inviteBanner">
+    <h1>Hi from Saif!</h1>
+    <p>
+      Working closely with developers as end users for years, I have developed a
+      clear understanding of their needs and possess the ability to collaborate
+      across engineering, marketing, partnerships, and customer success teams to
+      offer a greatly enhanced platform experience to our users. Having
+      represented the platform externally as an evangelist, advocate, and
+      trainer, I have learned to gather feedback from the user community, blend
+      it with my own research, and distill it for internal stakeholders.
+    </p>
+    <p>
+      I bring a combination of strong opinions backed by data and an open mind
+      that is always curious to learn. I aspire to leverage these skills to
+      create products that meet and exceed user expectations, ultimately making
+      their lives easier.
+    </p>
+  </div>
 </template>
 
-<style>
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+p {
+  font-family: Arial;
+  font-weight: 50;
+  font-size: large;
+  width: 65%;
+}
+
+div .inviteBanner {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+}
 </style>

5ffac9fe537cce633bf63d6e553e49de04a63dce
Saif <saifshine7@gmail.com>
2023-10-12 22:21:14 +0530

    Hyperlinks & Componentizing (#3)
    * 🐛 FIX: tab hint
    
    * 👌 IMPROVE: subtitle
    
    * 👌 IMPROVE: title and description
    
    * 👌 IMPROVE: typography and colors
    
    * 🐛 FIX: remove font
    
    * 👌 IMPROVE: compartmentalize - home and social links
    
    * 👌 IMPROVE: spread into intro component
    
    * 🚀 RELEASE: links

---
+++ src/components/Intro.vue
@@ -0,0 +1,39 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <p>
+      Working closely with developers as end users for years, I have developed a
+      clear understanding of their needs and possess the ability to collaborate
+      across engineering, marketing, partnerships, and customer success teams to
+      offer a greatly enhanced platform experience to our users. Having
+      represented the platform externally as an evangelist, advocate, and
+      trainer, I have learned to gather feedback from the user community, blend
+      it with my own research, and distill it for internal stakeholders.
+    </p>
+    <p>
+      I bring a combination of strong opinions backed by data and an open mind
+      that is always curious to learn. I aspire to leverage these skills to
+      create products that meet and exceed user expectations, ultimately making
+      their lives easier.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+@font-face {
+  font-family: "Urbanist";
+  src: url("../assets/fonts/Urbanist/Urbanist-VariableFont_wght.ttf");
+}
+
+p {
+  font-family: Urbanist;
+  font-weight: 50;
+  letter-spacing: 0.05em;
+  font-size: 1.3rem;
+  width: 65%;
+  color: #5c5470;
+}
+</style>
